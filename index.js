c9bd61f6a02615df3856d6e2ae4d2bf3cb2aef40
Shanmukh459 <saikrrish459@gmail.com>
2024-01-19 20:04:17 -0800

    implementation of the API

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 document.getElementById("get-color-btn").addEventListener("click", getColorScheme)
+let colorsArr = []
 
 function getColorScheme() {
-    const colorPicker = document.getElementById("color-picker").value
-    console.log(colorPicker)
-    fetch()
+    const colorPickerVal = document.getElementById("color-picker").value
+    const mode = document.getElementById("color-mode").value.toLowerCase()
+    console.log(mode)
+    const queryStr = `hex=${colorPickerVal.substring(1)}&mode=${mode}&count=5`
+    fetch(`https://www.thecolorapi.com/scheme?${queryStr}`)
+        .then(res => res.json())
+        .then(data => {
+            for(let color of data.colors) {
+                colorsArr.push(color.hex.value)
+            }
+        })
+    renderColors()
+}
+
+function renderColors() {
+    
 }
